85af036429ec18eb3e7682634e081ff6ff6c4d10
Ankit Bhatia <ankit95@berkeley.edu>
2016-11-01 12:25:39 -0700

    adds post schema/model/seeds

--- db/schema.rb
+++ db/schema.rb
@@ -10,6 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 0) do
+ActiveRecord::Schema.define(version: 20161101185950) do
+
+  create_table "posts", force: :cascade do |t|
+    t.string   "title"
+    t.text     "description"
+    t.string   "post_url"
+    t.string   "location"
+    t.string   "tags"
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
+  end
 
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,10 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+# Post Seeds
+Post.create(:title => "Post 1", :id => 1, :description => "Post 1 description", :post_url => "http://www.google.com", :location => "Norway", :tags => "post1,norway,google")
+Post.create(:title => "Post 2", :id => 1, :description => "Post 2 description", :post_url => "http://www.facebook.com", :location => "Sweden", :tags => "post2,sweden,facebook")
+Post.create(:title => "Post 3", :id => 1, :description => "Post 3 description", :post_url => "http://www.reddit.com", :location => "Russia", :tags => "post3,russia,reddit")
+Post.create(:title => "Post 4", :id => 1, :description => "Post 4 description", :post_url => "http://www.9gag.com", :location => "Greenland", :tags => "post4,greenland,9gag")
+Post.create(:title => "Post 5", :id => 1, :description => "Post 5 description", :post_url => "http://www.gmail.com", :location => "Iceland", :tags => "post5,iceland,gmail")
