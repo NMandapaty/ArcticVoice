7aff772f19f6be83394ee1aeaa0d8c85f2ca91b3
Nishant Mandapaty <nishantmandapaty@gmail.com>
2016-11-02 00:20:32 -0700

    Some other changes for rails 4

--- db/schema.rb
+++ db/schema.rb
@@ -1,3 +1,4 @@
+# encoding: UTF-8
 # This file is auto-generated from the current state of the database. Instead
 # of editing this file, please use the migrations feature of Active Record to
 # incrementally modify your database, and then regenerate this schema definition.
@@ -18,8 +19,8 @@ ActiveRecord::Schema.define(version: 20161101185950) do
     t.string   "post_url"
     t.string   "location"
     t.string   "tags"
-    t.datetime "created_at",  null: false
-    t.datetime "updated_at",  null: false
+    t.datetime "created_at"
+    t.datetime "updated_at"
   end
 
 end
