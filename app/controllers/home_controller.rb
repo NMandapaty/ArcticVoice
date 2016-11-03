ef84fe444f8828af5bcd5ef3302f70e2dd462be4
Timi <timifasubaa@gmail.com>
2016-11-03 09:41:11 +0000

    added search functionality skeleton.All that's left is to querydatabase tags for keywords

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,5 +1,12 @@
 class HomeController < ApplicationController
   def index
     @posts = Post.all
+    @search_keyword = params[:search]
+    if @search_keyword.nil?
+      @posts = Post.all
+    else
+      @posts = Post.all
+      #TODO(Timifasubaa): make it search the tag strings in Post for the search item.
+    end
   end
 end
