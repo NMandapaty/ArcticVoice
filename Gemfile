f352692fbd479ca468ef48cb078e05dd03f89dde
Ankit Bhatia <ankit95@berkeley.edu>
2016-11-16 22:19:30 -0800

    adds local images

--- Gemfile
+++ Gemfile
@@ -15,6 +15,7 @@ gem 'coffee-rails', '~> 4.1.0'
 gem 'simple_form', '~> 3.2'
 gem 'bootstrap-sass', '~> 3.3'
 gem 'ckeditor', '~> 4.1'
+gem 'paperclip'
 gem 'geocoder'
 gem 'gmaps4rails'
 
--- config/environments/development.rb
+++ config/environments/development.rb
@@ -35,7 +35,10 @@ Rails.application.configure do
   # Checks for improperly declared sprockets dependencies.
   # Raises helpful error messages.
   config.assets.raise_runtime_errors = true
-
+  
+  Paperclip.options[:image_magick_path] = "/opt/ImageMagick/bin"
+  Paperclip.options[:command_path] = "/opt/ImageMagick/bin"
+  
   # Raises error for missing translations
   # config.action_view.raise_on_missing_translations = true
 end
