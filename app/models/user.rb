ff7e670bae54b21be8f107931bedf64dde62643b
Ankit Bhatia <ankit95@berkeley.edu>
2016-11-17 21:15:54 -0800

    adds user profile pages to show their posts

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,5 @@ class User < ActiveRecord::Base
   # :confirmable, :lockable, :timeoutable, :trackable, :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+  has_many :posts
 end
