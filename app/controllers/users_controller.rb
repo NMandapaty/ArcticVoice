b6e476cc2e12afc62552ac649e455da753f2f7ef
Ankit Bhatia <ankit95@berkeley.edu>
2016-11-17 22:06:46 -0800

    allows users to see others' profile without logging in

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,6 +2,10 @@ class UsersController < ApplicationController
 	def show
 	  @user = User.find(params[:id])
 	  @user_posts = @user.posts
-	  @is_me = true if current_user.id == params[:id].to_i
+	  if current_user
+	  	@is_me = true if current_user.id == params[:id].to_i
+	  else
+	  	@is_me = false
+	  end
 	end
 end
