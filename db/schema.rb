e714eb605c86eb32faa05b0a49f460d861555948
Ankit Bhatia <ankit95@berkeley.edu>
2016-11-28 23:48:32 -0800

    fixes user schema lat/long

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,21 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161116215714) do
+ActiveRecord::Schema.define(version: 20161129063748) do
+
+  create_table "bootsy_image_galleries", force: :cascade do |t|
+    t.integer  "bootsy_resource_id"
+    t.string   "bootsy_resource_type"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "bootsy_images", force: :cascade do |t|
+    t.string   "image_file"
+    t.integer  "image_gallery_id"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
   create_table "ckeditor_assets", force: :cascade do |t|
     t.string   "data_file_name",               null: false
@@ -62,8 +76,7 @@ ActiveRecord::Schema.define(version: 20161116215714) do
     t.string   "name",                   default: "",    null: false
     t.boolean  "is_researcher",          default: false, null: false
     t.string   "phone"
-    t.decimal  "latitude"
-    t.decimal  "longitude"
+    t.string   "location"
     t.string   "email",                  default: "",    null: false
     t.string   "encrypted_password",     default: "",    null: false
     t.string   "reset_password_token"
@@ -71,6 +84,8 @@ ActiveRecord::Schema.define(version: 20161116215714) do
     t.datetime "remember_created_at"
     t.datetime "created_at",                             null: false
     t.datetime "updated_at",                             null: false
+    t.decimal  "latitude"
+    t.decimal  "longitude"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
