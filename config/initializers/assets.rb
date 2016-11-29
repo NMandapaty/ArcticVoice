7f1d45602f7741202c77d10c45200f648eb5fb2d
Pratham Alag <eeshanagarwal@berkeley.edu>
2016-11-29 01:14:42 -0800

    adding back the css files

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -4,7 +4,7 @@
 Rails.application.config.assets.version = '1.0'
 Rails.application.config.assets.precompile += %w( bootstrap.min.css )
 Rails.application.config.assets.precompile += %w( styles.css )
-Rails.application.config.assets.precompile += %w( stolen.css )
+Rails.application.config.assets.precompile += %w( homepage.css )
 Rails.application.config.assets.precompile += %w( bootstrap.min.js )
 Rails.application.config.assets.precompile += %w( map.js )
 Rails.application.config.assets.precompile += %w( users.scss )
