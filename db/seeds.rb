b4287b3479cba4839e973b599dd1e714bc4ca7a2
Timi <timifasubaa@gmail.com>
2016-11-29 07:08:42 +0000

    data in the

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,7 +7,7 @@
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
 # Post Seeds
-post1 = Post.create(:title => "Post 1", :body => "Post 1 body", :location => "Norway", :tags => "post1,norway,google")
+post1 = Post.create(:title => "The ice level is lower than it was this time last year", :body => "I was here for a hike this time last year and the ice was at least a meter higher than it is right now!", :location => "Norway", :tags => "post1,norway,google")
 post2 = Post.create(:title => "Post 2", :body => "Post 2 body", :location => "Sweden", :tags => "post2,sweden,facebook")
 Post.create(:title => "Post 3", :body => "Post 3 body", :location => "Russia", :tags => "post3,russia,reddit")
 Post.create(:title => "Post 4", :body => "Post 4 body", :location => "Greenland", :tags => "post4,greenland,9gag")
