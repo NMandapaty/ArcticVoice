90264438fb443e72764a20a53862f2b1a9ebd737
Pratham Alag <eeshanagarwal@berkeley.edu>
2016-11-29 01:32:22 -0800

    fixing heroku db situation

--- db/schema.rb
+++ db/schema.rb
@@ -83,7 +83,7 @@ ActiveRecord::Schema.define(version: 20161129063748) do
     t.datetime "created_at",                             null: false
     t.datetime "updated_at",                             null: false
     t.decimal  "latitude"
-    t.decimal  "longitude"
+    t.decimal  "longitude" 
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
